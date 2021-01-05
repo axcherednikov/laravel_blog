296e0bd98d78456ce1f1ed3be85494afcf1b74b0
Excent163 <agel63@mail.ru>
2021-01-05 18:34:27 +0300

    Show cloud tags by tasks only auth user

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,16 +1,20 @@
-<!-- /.blog-main -->
 <aside class="col-md-4 blog-sidebar">
+
     <div class="p-4 mb-3 bg-light rounded">
+
         <h4 class="font-italic">Облако тэгов</h4>
         <br>
-        <h5>Тэги задач</h5>
-        @include('tasks.tags', ['tagsTask' => $tagsTaskCloud])
-        <br>
-        <h5>Тэги постов</h5>
-        @php
 
-        @endphp
+        @auth()
+            <h5>Тэги задач</h5>
+
+            @include('tasks.tags', ['tagsTask' => $tagsTaskCloud])
+            <br>
+        @endauth
+
+        <h5>Тэги постов</h5>
+
         @include('posts.tags', ['tagsPost' => $tagsPostCloud])
     </div>
+
 </aside>
-<!-- /.blog-sidebar -->
